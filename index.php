5539013e8a72d4118703f6d2dfe98158fee9136d
Howard <xyz97128@gmail.com>
2022-10-05 19:57:23 +0800

    '姓名重複顯示'

--- index.php
+++ index.php
@@ -9,48 +9,49 @@
     <script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.1/js/bootstrap.min.js' integrity='sha512-vyRAVI0IEm6LI/fVSv/Wq/d0KUfrg3hJq2Qz5FlfER69sf3ZHlOrsLriNm49FxnpUGmhx+TaJKwJ+ByTLKT+Yg==' crossorigin='anonymous'></script>
 </head>
 <body>
-    <p>
-        <span>班級</span>
-        <select name="class" id="class">
-            <option value="請選擇">請選擇</option>
-            <option value="資三a">資三A</option>
-            <option value="資三b">資三B</option>
-            <option value="資三c">資三C</option>
-            <option value="資三d">資三D</option>
-            <option value="資三e">資三E</option>
-            <option value="資三f">資三F</option>
-        </select>
-    </p>
-    <p>
-        <span>月份</span>
-        <select name="month" id="month">
-            <option value="9">九月</option>
-            <option value="10">十月</option>
-            <option value="11">十一月</option>
-            <option value="12">十二月</option>
-            <option value="1">一月</option>
-            <option value="2">二月</option>
-            <option value="3">三月</option>
-            <option value="4">四月</option>
-            <option value="5">五月</option>
-            <option value="6">六月</option>
-            <option value="7">七月</option>
-            <option value="8">八月</option>
-        </select>
-    </p>
-    <table border="1" class="">
-        <thead>
-            <tr align="center">
-                <th>學號</th>
-                <th>姓名</th>
-                <th>功能</th>
-            </tr>
-        </thead>
-        <tbody id="sList">
-            <tr><td colspan="3">尚未選擇班級！</td></tr>
-        </tbody>
-    </table>
-    
+    <div class="container">
+        <p>
+            <span>班級</span>
+            <select name="class" id="class">
+                <option value="請選擇">請選擇</option>
+                <option value="資三a">資三A</option>
+                <option value="資三b">資三B</option>
+                <option value="資三c">資三C</option>
+                <option value="資三d">資三D</option>
+                <option value="資三e">資三E</option>
+                <option value="資三f">資三F</option>
+            </select>
+        </p>
+        <p>
+            <span>月份</span>
+            <select name="month" id="month">
+                <option value="9">九月</option>
+                <option value="10">十月</option>
+                <option value="11">十一月</option>
+                <option value="12">十二月</option>
+                <option value="1">一月</option>
+                <option value="2">二月</option>
+                <option value="3">三月</option>
+                <option value="4">四月</option>
+                <option value="5">五月</option>
+                <option value="6">六月</option>
+                <option value="7">七月</option>
+                <option value="8">八月</option>
+            </select>
+        </p>
+        <table border="1" class="table">
+            <thead>
+                <tr align="center">
+                    <th>學號</th>
+                    <th>姓名</th>
+                    <th>功能</th>
+                </tr>
+            </thead>
+            <tbody id="sList">
+                <tr><td colspan="3">尚未選擇班級！</td></tr>
+            </tbody>
+        </table>
+    </div>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js' integrity='sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==' crossorigin='anonymous'></script>
     <script src="./js/script.js"></script>
 </body>
--- js/script.js
+++ js/script.js
@@ -7,43 +7,21 @@ $('#class').change((e) => {
             url: './db/loadData.php',
             data: {class: value},
             type: 'POST',
-            dataType: 'json',
+            dataType: 'JSON',
             success: (result) => {
-                // console.log(result)
-                Object.values(result).forEach((v) => {
+                Object.values(result).forEach((v) => {                  
                     $('#sList').append(
                         '<tr>' +
+                        `<td>${v[0]}</td>` +
                         `<td>${v[1]}</td>` +
-                        `<td>${v[2]}</td>` +
-                        `<td>
-                        <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-查看資料
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">${v[2]} 同學</h5>
-        <button type="button" c lass="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
-      </div>
-      <div class="modal-body">
-        使用者自定義內容
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-                        </td>` +
+                        `<td>${studentDialog(v[1])}</td>` +
                         '</tr>'
                     )
                 })
             },
+            error: () => {
+              $('#sList').append('<tr><td colspan=3>尚未建立班級資料庫</td></tr>')      
+            }
         })
     }
     else
@@ -51,7 +29,35 @@ $('#class').change((e) => {
 })
 
 
-function studentDialog()
-{
+function studentDialog(name)
+{  
+  console.log(name)
+  return `<!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  查看學生資料
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">${name} 同學</h5>
+          <button type="button" c lass="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
+        </div>
+        <div class="modal-body">
+          ${optionDropDown()}
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+}
 
+function optionDropDown()
+{
+  return "function test"
 }
